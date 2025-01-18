8e2f95825cc5719b1d1e2fd5bcafcf2da4861118
virgilio1974 <vitrgilio1974@gmail.com>
2025-01-18 09:58:56 +0100

    MIGRATION merchant model

---
+++ db/migrate/20250118085124_create_merchants.rb
@@ -0,0 +1,17 @@
+# db/migrate/YYYYMMDDHHMMSS_create_merchants.rb
+class CreateMerchants < ActiveRecord::Migration[8.0]
+  def change
+    create_table :merchants, id: false do |t|
+      t.string :id, primary_key: true
+      t.string :reference, null: false
+      t.string :email, null: false
+      t.date :live_on, null: false
+      t.integer :disbursement_frequency, null: false
+      t.integer :minimum_monthly_fee_cents, null: false, default: 0
+
+      t.timestamps
+
+      t.index :reference, unique: true
+    end
+  end
+end
