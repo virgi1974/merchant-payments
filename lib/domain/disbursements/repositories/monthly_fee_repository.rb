cce67f9ad9a850cd71c810a31f29f921b250220a
virgilio1974 <vitrgilio1974@gmail.com>
2025-01-25 21:46:41 +0100

    Logic for monthly_fee_adjustment

---
+++ lib/domain/disbursements/repositories/monthly_fee_repository.rb
@@ -0,0 +1,35 @@
+module Domain
+  module Disbursements
+    module Repositories
+      class MonthlyFeeRepository
+        DISBURSEMENT_MODEL = Infrastructure::Persistence::ActiveRecord::Models::Disbursement
+        MONTHLY_FEE_ADJUSTMENT_MODEL = Infrastructure::Persistence::ActiveRecord::Models::MonthlyFeeAdjustment
+
+
+        def total_fees_for_month(merchant, month, year)
+          start_date = Date.new(year, month, 1)
+          end_date = start_date.end_of_month
+
+          DISBURSEMENT_MODEL
+            .where(merchant: merchant)
+            .where(created_at: start_date..end_date)
+            .sum(:fees_amount_cents)
+        end
+
+        def create_monthly_adjustment(merchant:, amount:, month:, year:)
+          MONTHLY_FEE_ADJUSTMENT_MODEL.create!(
+            merchant: merchant,
+            amount_cents: amount,
+            month: month,
+            year: year
+          )
+        end
+
+        def adjustment_exists?(merchant, month, year)
+          MONTHLY_FEE_ADJUSTMENT_MODEL
+            .exists?(merchant: merchant, month: month, year: year)
+        end
+      end
+    end
+  end
+end
---
+++ lib/domain/disbursements/services/monthly_fee_tracker.rb
@@ -0,0 +1,36 @@
+module Domain
+  module Disbursements
+    module Services
+      class MonthlyFeeTracker
+        def initialize(repository = Repositories::MonthlyFeeRepository.new)
+          @repository = repository
+        end
+
+        def process_merchant(merchant, month, year)
+          return if @repository.adjustment_exists?(merchant, month, year)
+
+          total_fees = calculate_monthly_fees(merchant, month, year)
+          minimum_fee = merchant.minimum_monthly_fee_cents
+          fee_difference = minimum_fee - total_fees
+
+          create_adjustment(merchant, fee_difference, month, year) if fee_difference.positive?
+        end
+
+        private
+
+        def calculate_monthly_fees(merchant, month, year)
+          @repository.total_fees_for_month(merchant, month, year)
+        end
+
+        def create_adjustment(merchant, amount, month, year)
+          @repository.create_monthly_adjustment(
+            merchant: merchant,
+            amount: amount,
+            month: month,
+            year: year
+          )
+        end
+      end
+    end
+  end
+end
